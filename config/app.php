d512dfb234e3fe16356318ec465f5d04f7f4b3df
mark <xeneas@hotmail.com>
2016-10-12 09:47:30 -0500

    1.1 Traducido al español y Timezone

--- config/app.php
+++ config/app.php
@@ -36,10 +36,11 @@ return [
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
-    |
+    | // Changet -> timezone
+    |    http://php.net/manual/es/timezones.america.php
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Mexico_City',
 
     /*
     |--------------------------------------------------------------------------
@@ -49,10 +50,10 @@ return [
     | The application locale determines the default locale that will be used
     | by the translation service provider. You are free to set this value
     | to any of the locales which will be supported by the application.
-    |
+    | //Changet es
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------

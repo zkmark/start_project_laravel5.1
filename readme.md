e28f3eaa5d83e89dcd30b47dac35066db40d4c44
mark <xeneas@hotmail.com>
2016-10-12 16:08:26 -0500

    Agregado bootstrap

--- readme.md
+++ readme.md
@@ -6,9 +6,22 @@
 [![Latest Unstable Version](https://poser.pugx.org/laravel/framework/v/unstable.svg)](https://packagist.org/packages/laravel/framework)
 [![License](https://poser.pugx.org/laravel/framework/license.svg)](https://packagist.org/packages/laravel/framework)
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, queueing, and caching.
+## 
+Laravel 5.1 LTS listo para comenzar proyectos incluye
+- Traduccion al español
+- Laravelcollective
+- Zona Horaria Mexico
 
-Laravel is accessible, yet powerful, providing powerful tools needed for large, robust applications. A superb inversion of control container, expressive migration system, and tightly integrated unit testing support give you the tools you need to build any application with which you are tasked.
+## Instalacion
+Despues de tener instalado composer ejecutar dentro del proyecto (accede con cd nombre)
+composer install
+
+Edita el .env.example copiandolo y quitandole el .example
+
+Crea tu key
+php artisan key:generate
+
+Ejecuta php artisan serve
 
 ## Official Documentation
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,33 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="description" content="">
+	<meta name="copyright" content="© 2016">
+	<meta name="author" content="">
+	<title>Laravel</title>
+ 
+	{!! Html::style('css/bootstrap.css') !!}
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+	<h1>Funcionando...</h1>
+	<p>Incluye jquery, bootstrap 3.3.7, bootstrap validator</p>
+ 
+	<!-- Scripts -->
+	{!! Html::script('js/jquery-3.1.1.min.js') !!}
+	{!! Html::script('js/bootstrap.min.js') !!}
+	{!! Html::script('js/validator.min.js') !!}
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+</body>
 </html>

94e236760e90d83f4d8f5733fee3f6750565623d
mark <xeneas@hotmail.com>
2016-10-12 09:55:26 -0500

    1.2 Agregado laravel collective

--- config/app.php
+++ config/app.php
@@ -146,6 +146,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //My Providers
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -193,6 +196,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        //My aliases
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
